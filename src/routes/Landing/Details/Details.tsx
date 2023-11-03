6ffb85a44241c950b8ffaaf5c768b88386e2384c
Devin O'Brien <61803565+devinobrien-css@users.noreply.github.com>
2023-11-03 00:35:37 -0400

    updated details

--- src/routes/Landing/Details/Details.tsx
+++ src/routes/Landing/Details/Details.tsx
@@ -5,6 +5,10 @@ export const Details = () => {
       <p className="font-jua text-56 text-light-grey">
         idk. something should go here. maybe an explanation
       </p>
+      <p className="font-jua text-56 text-light-grey">
+        something to get the people going
+      </p>
+      <p className="font-jua text-56 text-light-grey">or a pun.</p>
     </div>
   );
 };
