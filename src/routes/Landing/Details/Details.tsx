88ca56ede9c030d247928f1546addbab71ecfa9c
Devin O'Brien <61803565+devinobrien-css@users.noreply.github.com>
2023-11-07 23:32:35 -0500

    sick n' tired of the IAU's shit :100:

--- src/routes/Landing/Details/Details.tsx
+++ src/routes/Landing/Details/Details.tsx
@@ -1,14 +1,40 @@
 export const Details = () => {
   return (
-    <div className="relative min-h-screen p-12">
-      <p className="font-jua text-56 text-light-grey">Details</p>
+    <div className="relative flex flex-col gap-y-12 pb-96">
       <p className="font-jua text-56 text-light-grey">
-        idk. something should go here. maybe an explanation
+        To the International Astronomical Union:
+        <br />
+        We, the people of Earth, are sick and tired of your shenanigans. We are
+        sick and tired of your arbitrary rules and regulations. We are sick and
+        tired of your double-standards. We are sick and tired of your disrespect
+        for Pluto.
       </p>
       <p className="font-jua text-56 text-light-grey">
-        something to get the people going
+        In short, we want Pluto back. We want it back as a planet. We want it
+        back as a planet, and we want it back now.
       </p>
-      <p className="font-jua text-56 text-light-grey">or a pun.</p>
+      <p className="font-jua text-56 text-light-grey">
+        Pluto is a planet. It's not a planet. It's a planet again. It's not a
+        planet again.
+      </p>
+      <p className="font-jua text-56 text-light-grey">
+        Stop the madness. Bring Pluto back. Or else.
+      </p>
     </div>
   );
 };
+
+/*
+The history of Pluto's discovery began in the mid-19th century when the
+mathematical astronomy of Urbain Le Verrier predicted the existence and
+position of Neptune, prompting astronomers to speculate about planets
+beyond its orbit. In 1906, Percival Lowell, a wealthy Bostonian who had
+founded the Lowell Observatory in Flagstaff, Arizona in 1894, started an
+extensive project in search of a possible ninth planet, which he termed
+"Planet X". By 1909, Lowell and William H. Pickering had suggested
+several possible celestial coordinates for such a planet. Lowell and his
+observatory conducted his search until his death in 1916, but to no
+avail. Unknown to Lowell, his surveys had captured two faint images of
+Pluto on March 19 and April 7, 1915, but they were not recognized for
+what they were.
+*/
