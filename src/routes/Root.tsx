9ceb747f342940c3460e41fbe3938550067d8fed
Em Gore <emilygorelik2000@gmail.com>
2023-11-14 17:19:14 -0500

    not shit nav bar

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -2,14 +2,14 @@ import { Link, Outlet } from 'react-router-dom';
 export const Root = () => {
   return (
     <div className="relative">
-      <div className="absolute right-2 top-2 z-50 flex gap-x-2">
-        <Link to="/" className="underline">
+      <div className="flex justify-end w-full gap-4 bg-accent text-primary p-2 text-lg">
+        <Link to="/" className="uppercase p-2 rounded hover:bg-accent-focus font-bold">
           home
         </Link>
-        <Link to="/timeline" className="underline">
+        <Link to="/timeline" className="uppercase p-2 rounded hover:bg-accent-focus font-bold">
           timeline
         </Link>
-        <Link to="/merch" className="underline">
+        <Link to="/merch" className="uppercase p-2 rounded hover:bg-accent-focus font-bold">
           merch
         </Link>
       </div>
