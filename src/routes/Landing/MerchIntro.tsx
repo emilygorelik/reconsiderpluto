e88e6c3934917775109b948c69d4c994c02b81c2
Emily Gorelik <egorelik@jahnelgroup.com>
2023-11-17 11:59:15 -0500

    cleaning up

--- src/routes/Landing/MerchIntro.tsx
+++ src/routes/Landing/MerchIntro.tsx
@@ -23,38 +23,32 @@ const SquiglySvg = ({ className }: SquiglySvgProps) => {
 
 export const Merch = () => {
   return (
-    <div className="relative min-h-screen">
-      <div className="flex">
-        <div className="relative my-auto w-5/6 bg-secondary text-primary">
-          <SquiglySvg className="absolute left-[99%] top-0 h-full" />
-          <div className="p-2 sm:p-8 md:p-12">
-            <h2 className="uppercase">
-              Are you comfortable with 8 planets? <br /> Are you?! Cause we're
-              not.
-            </h2>
-            <p>
-              The reclassification of Pluto as a dwarf planet in 2006 was a
-              decision that continues to be met with resistance and controversy.
-              It is argued that Pluto's removal as a planet was wrongfully based
-              on arbitrary criteria, a double standard in their application, and
-              a disregard for the historical and cultural significance of this
-              celestial body.
-            </p>
-            <div className="flex items-end justify-end">
-              <Button
-                onClick={() => toast.warn('*italian accent* ita no worka yet')}
-                className="px-8"
-              >
-                Donate Here
-              </Button>
+    <div className="my-4 w-screen bg-secondary p-4 text-primary sm:w-5/6">
+      <SquiglySvg className="invisible absolute left-[99%] top-0 h-full sm:visible" />
+      <div className="flex flex-col p-2 sm:p-8 md:p-12">
+        <h2 className="uppercase">
+          Are you comfortable with 8 planets? <br /> Are you?! Cause we're not.
+        </h2>
+        <p>
+          The reclassification of Pluto as a dwarf planet in 2006 was a decision
+          that continues to be met with resistance and controversy. It is argued
+          that Pluto's removal as a planet was wrongfully based on arbitrary
+          criteria, a double standard in their application, and a disregard for
+          the historical and cultural significance of this celestial body.
+        </p>
+        <div className="flex flex-col-reverse items-end justify-end">
+          <Button
+            onClick={() => toast.warn('*italian accent* ita no worka yet')}
+            className="px-8"
+          >
+            Donate Here
+          </Button>
 
-              <img
-                src="/assets/img/logo.png"
-                alt="give us money"
-                className="w-1/3 animate-wiggle-more ease-in-out animate-duration-[3500ms] animate-infinite"
-              />
-            </div>
-          </div>
+          <img
+            src="/assets/img/logo.png"
+            alt="give us money"
+            className="w-1/3 animate-wiggle-more ease-in-out animate-duration-[3500ms] animate-infinite"
+          />
         </div>
       </div>
     </div>
--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -2,7 +2,7 @@ import { Link, Outlet } from 'react-router-dom';
 export const Root = () => {
   return (
     <div className="relative">
-      <div className="flex w-full justify-end gap-4 bg-accent p-2 text-lg text-primary fixed z-50">
+      <div className="fixed z-50 flex w-full justify-end gap-4 bg-accent p-2 text-lg text-primary">
         <Link
           to="/"
           className="rounded p-2 font-bold uppercase hover:bg-accent-focus"
@@ -22,7 +22,7 @@ export const Root = () => {
           timeline
         </Link> */}
       </div>
-      <br/> <br/>
+      <br /> <br />
       <Outlet />
     </div>
   );
